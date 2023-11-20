f5016fb9b06c4a8128eccba8fed471acef3666d2
michael <michaeldowd2@gmail.com>
2023-11-20 21:37:32 +0000

    add analysis prompts and fix vectorstore

--- src/analyse.py
+++ src/analyse.py
@@ -22,10 +22,10 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 import logging
-from .queries import BINGQ_Factors
-from .queries import LLMQ_Factors
-from .queries import BINGQ_Effect
-from .queries import LLMQ_Effect
+from .queries import Factors_Q0
+from .queries import Effect_Q0
+from .queries import Effect_Q1
+from .queries import Effect_Q2
 
 def Analyse(config_file, config, timestamp):
     no_factors= config['no_factors']
@@ -34,41 +34,68 @@ def Analyse(config_file, config, timestamp):
     
     res = {}
     for subject in config['subjects']:
-        res[subject] = {'factors':{}} 
-    
-    for subject in config['subjects']:
+        res[subject] = {} # create dictionary for results
         analysis_id = subject.replace(' ', '_')
-        factors = FindFactors(subject, no_factors, no_links, model_props, config_file, analysis_id)
-        logging.info('factors returned')
-        logging.info(factors)
-        for factor in factors:
-            res[subject]['factors'][factor]={}
-        SaveRes(res, config_file, timestamp)
+        for direction in ['increase', 'decrease']:
+            res[subject][direction] = {} # create dictionary for results
+            factors = FindFactors(subject, Factors_Q0, direction, no_factors, no_links, model_props, config_file, analysis_id)
+            logging.info(factors)
+            f_no = 0
+            for factor in factors:
+                if isNotANumber(factor):
+                    res[subject][direction][factor] = {} # create dictionary for results
+                    SaveRes(res, config_file, timestamp)
+                    q_no, bing_dl = 0, True
+                    for query in [Effect_Q0, Effect_Q1, Effect_Q2]: #loop through effect queries
+                        if q_no > 0:
+                            bing_dl = False
+                        q_name = str(query.__name__)
+                        prefix = direction + '_F' + str(f_no)
+                        effect = AnalyseFactor(subject, factor, query, direction, no_links, model_props, bing_dl, config_file, analysis_id, prefix)
+                        res[subject][direction][factor][q_name] = effect
+                        SaveRes(res, config_file, timestamp)
+                        q_no+=1
+                    f_no += 1
 
-        factorid = 0
-        for factor in factors:
-            factor_increase = AnalyseFactor(subject, factor, no_links, model_props, config_file, analysis_id, factorid)
-            if (factor_increase != None):
-                logging.info('factor: ' + str(factorid) + ' returned')
-                logging.info(factor_increase)
-                res[subject]['factors'][factor]['increase'] = factor_increase
-                SaveRes(res, config_file, timestamp)
-            factorid += 1
-
-def FindFactors(subject, no_factors, no_links, model_props, config_file, analysis_id):
-    bq = BINGQ_Factors(subject, no_factors)
-    q, t = LLMQ_Factors(subject, no_factors)
-    SearchBingGetLinksAndSave(bq, config_file, analysis_id, 'factors', no_links)
-    answer = LoadContextAndRunLLM(q, t, model_props, 'json_arr.gbnf', config_file, analysis_id, 'factors')
+def FindFactors(subject, query, direction, no_factors, no_links, model_props, config_file, analysis_id):
+    bs, q, t = query()
+    bs = (Parameterise(b, subject, '', no_factors, direction) for b in bs)
+    q = Parameterise(q, subject, '', no_factors, direction) 
+    SearchBingGetLinksAndSave(bs, no_links, config_file, analysis_id, direction + '_factors')
+    answer = LoadContextAndRunLLM(q, t, model_props, 'json_arr.gbnf', config_file, analysis_id, direction + '_factors')
+    print('factor result')
+    print(answer)
     return ParseJSONResult(answer)
 
-def AnalyseFactor(subject, factor, no_links, model_props, config_file, analysis_id, factorid):
-    bq = BINGQ_Effect(subject, factor)
-    q, t = LLMQ_Effect(subject, factor)
-    SearchBingGetLinksAndSave(bq, config_file, analysis_id, 'factor'+str(factorid), no_links)
-    answer = LoadContextAndRunLLM(q, t, model_props, 'json.gbnf', config_file, analysis_id, 'factor'+str(factorid))
-    return ParseJSONResult(answer)
+def AnalyseFactor(subject, factor, query, direction, no_links, model_props, bing_dl, config_file, analysis_id, prefix):
+    bs, q, t = query()
+    bs = (Parameterise(b, subject, factor, '', direction) for b in bs)
+    q = Parameterise(q, subject, factor, '', direction) 
+    if bing_dl:
+        SearchBingGetLinksAndSave(bs, no_links, config_file, analysis_id, prefix)
+    answer = LoadContextAndRunLLM(q, t, model_props, 'json.gbnf', config_file, analysis_id, prefix)
+    return answer['result']
 
+def Parameterise(inp, subject = '', factor = '', no_factors = '', direction = ''):
+    inp = inp.replace('{subject}', subject)
+    inp = inp.replace('{factor}', factor)
+    inp = inp.replace('{no_factors}', str(no_factors))
+    inp = inp.replace('{direction}', direction)
+    return inp
+
+def isNotANumber(factor):
+    try:
+        float(factor)
+        return False # not a valid factor if it can be converted to a float
+    except:
+        pass
+    try:
+        int(factor)
+        return False
+    except:
+        pass
+    return True
+    
 def LoadModel(model_props, grammar):
     model_path = os.path.join(os.getcwd(), 'models', model_props['name'])
     grammar_path = os.path.join(os.getcwd(), 'models', grammar)
@@ -94,8 +121,7 @@ def SaveRes(res, config_file, timestamp):
         json.dump(res, f, indent=4)
         logging.info('saved result file to: ' + fname)
 
-
-def RequestURLAndSave(url, config_file, analysis_id, doc_prefix, doc_id):
+def RequestURLAndSave(url, save, config_file, analysis_id, doc_prefix, doc_id):
     try:
         headers = {
             "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582" 
@@ -109,35 +135,53 @@ def RequestURLAndSave(url, config_file, analysis_id, doc_prefix, doc_id):
             os.makedirs(path)
             logging.info('created html output path at: ' + path)
         
-        fname = os.path.join(path, str(doc_id) + '.html')
-        with open(fname, "w", encoding="utf-8") as f:
-            f.write(str(response))
-        logging.info('saved html file to: ' + fname)
+        if save:
+            fname = os.path.join(path, str(doc_id) + '.html')
+            with open(fname, "w", encoding="utf-8") as f:
+                f.write(str(response))
+            logging.info('saved html file to: ' + fname)
         return response
     except Exception as e:
         logging.exception("error querying url or saving result")
         logging.exception(traceback.format_exc())
         raise Exception(e) 
 
-def SearchBingGetLinksAndSave(query, config_file, analysis_id, doc_prefix, no_links):
+def SearchBingGetLinksAndSave(queries, no_links, config_file, analysis_id, doc_prefix):
     try:
-        logging.info('running bing search for query: ' + query)
-        url = "https://www.bing.com/search?form=QBRE&q="+query.replace(' ', '+')
-        response = RequestURLAndSave(url, config_file, analysis_id, doc_prefix, 0)
-        soup = BeautifulSoup(response, 'lxml')
-        links = []
-        for container in soup.select('.b_algo h2 a'):
-            links.append(container['href'])
-        links = links[:no_links]
-        doc_id = 1
-        for link in links:
-            RequestURLAndSave(link, config_file, analysis_id, doc_prefix, doc_id)
-            doc_id += 1
-        logging.info('finished running and saving results for query')
+        doc_id = 0
+        for query in queries:
+            logging.info('running bing search for query: ' + query)
+            url = "https://www.bing.com/search?form=QBRE&q="+query.replace(' ', '+')
+            response = RequestURLAndSave(url, False, config_file, analysis_id, doc_prefix, doc_id)
+            soup = BeautifulSoup(response, 'lxml')
+            links = []
+            for container in soup.select('.b_algo h2 a'):
+                actURL = BingToActualURL(container['href']) # need to find the actual url
+                if actURL is not None:
+                    links.append(actURL)
+            links = links[:no_links]
+            for link in links:
+                RequestURLAndSave(link, True, config_file, analysis_id, doc_prefix, doc_id)
+                doc_id += 1
+            logging.info('finished running and saving results for query')
     except Exception as e:
         logging.exception("error running bing search and save results")
         logging.exception(traceback.format_exc())
         raise Exception(e) 
+
+def BingToActualURL(url):
+    headers = {
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582" 
+        }
+    response = requests.get(url, headers = headers).text
+    start_ind = response.index('var u = "')
+    if start_ind >= 0:
+        response = response[start_ind+9:]
+        end_ind = response.find('";')
+        if end_ind >=0:
+            response = response[:end_ind]
+            return response
+    return None
 
 def CreateVectorDB(config_file, analysis_id, prefix):
     try:
@@ -147,7 +191,7 @@ def CreateVectorDB(config_file, analysis_id, prefix):
         loader = DirectoryLoader(vector_db_path, loader_cls=BSHTMLLoader, loader_kwargs={'open_encoding':'utf8'})
         documents = loader.load()
         logging.info('splitting documents')
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 750, chunk_overlap = 50)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 50)
         docs = text_splitter.split_documents(documents)
         logging.info('generating embeddings')
         embeddings = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
@@ -166,7 +210,7 @@ def LoadContextAndRunLLM(query, template, model_props, grammar, config_file, ana
         llm=model,
         chain_type='stuff',
         retriever=vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k":5}),
-        return_source_documents = False,
+        return_source_documents = True,
         chain_type_kwargs = {"prompt":prompt}
     )
     res = qa({"query": query})
--- src/queries.py
+++ src/queries.py
@@ -1,9 +1,71 @@
-def BINGQ_Factors(subject, no_factors):
-    bq = 'what causes ' + str(no_factors) + ' the price of ' + subject + ' to increase'
+def BINGQ_Factors(subject, no_factors, direction):
+    bq = 'what causes ' + str(no_factors) + ' the price of ' + subject + ' to ' + direction
     return bq
 
-def LLMQ_Factors(subject, no_factors):
-    q = 'what are the' + str(no_factors) + ' single biggest factors that cause the ' + subject + ' to increase'
+def Factors_Q0():
+    b = [
+        "top {no_factors}  factors that cause the price of {subject} to {direction}"
+    ]
+    p = "what are the {no_factors} single biggest factors that cause the price of {subject} to {direction}"
+    t = """
+Answer the question at the end using the following context. Answer in list form.
+<context>
+{context}
+</context>
+Q: {question}? A:
+"""
+    return b, p, t
+
+def Effect_Q0():
+    b = [
+        "latest news on {factor} relating to {subject}",
+        "Reddit update on {factor} relating to {subject}",
+        "Bloomberg news on {factor} relating to {subject}"
+    ]
+    p = "In the near future is {factor} relating to {subject} likely to occur"
+    t = """
+Use only the context below to give a one word answer to the question at the end. Answer in json form with the keys "answer" and "explanation".
+<context>
+{context}
+</context>
+Q: {question}? A:
+"""
+    return b, p, t
+
+def Effect_Q1():
+    b = [
+        "latest news on {factor} relating to {subject}",
+        "Reddit update on {factor} relating to {subject}",
+        "Bloomberg news on {factor} relating to {subject}"
+    ]
+    p = "In the near future how likely is it that {factor} will cause the price of {subject} to {direction}"
+    t = """
+Use only the context below to give a one word answer to the question at the end. Answer in json form with the keys "answer" and "explanation".
+<context>
+{context}
+</context>
+Q: {question}? A:
+"""
+    return b, p, t
+
+def Effect_Q2():
+    b = [
+        "latest news on {factor} relating to {subject}",
+        "Reddit update on {factor} relating to {subject}",
+        "Bloomberg news on {factor} relating to {subject}"
+    ]
+    p = "Is the price of {subject} likely to {direction} due to {factor} in the near"
+    t = """
+Use only the context below to give a one word answer to the question at the end. Answer in json form with the keys "answer" and "explanation".
+<context>
+{context}
+</context>
+Q: {question}? A:
+"""
+    return b, p, t
+
+def LLMQ_Factors(subject, no_factors, direction):
+    q = 'what are the' + str(no_factors) + ' single biggest factors that cause the ' + subject + ' to ' + direction
     t = """
 Answer the question at the end using the following context. Answer in list form.
 <context>
